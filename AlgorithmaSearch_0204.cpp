bd6c4955f2bd4d982ab55d0ff2cc534e24596350
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-04-04 16:13:24 +0700

    commit7

--- AlgorithmaSearch_0204.cpp
+++ AlgorithmaSearch_0204.cpp
@@ -16,4 +16,9 @@ void input()
         else
             cout <<"\nArray should have minimum 1 and maximum 20 elements.\n\n"; 
     }
+
+
+    cout << "\n---------------------\n";
+    cout << "enter array elements \n";
+    cout << "---------------------\n";
 }
